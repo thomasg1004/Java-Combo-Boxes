c5860259baab04afa823c3f2dd9957b55d19ec34
Thomas <59719750+thomasg1004@users.noreply.github.com>
2021-04-27 17:34:11 -0500

    Add files via upload

---
+++ ComboBoxesTester.java
@@ -0,0 +1,26 @@
+//Run this code
+import javax.swing.JFrame;
+
+public class ComboBoxesTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+			// instantiate a frame
+			// from the class JFrame
+		JFrame frame = new JFrame("Fun with combo boxes");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+		
+			// the primary panel is added
+			// to the frame and made visible
+		frame.getContentPane().add(new ComboBoxesGUI());
+		frame.pack();
+		frame.setVisible(true);
+		
+
+
+	}	// end method main
+
+}	// end class ComboBoxesTester
